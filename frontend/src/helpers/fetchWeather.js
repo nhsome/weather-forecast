d23ba4d9ab783ae2874c507657a64e0dde16f68c
Alex Shor <nhsome@ya.ru>
2020-02-23 22:24:18 +0300

    Fixed get geoposition

--- frontend/src/helpers/fetchWeather.js
+++ frontend/src/helpers/fetchWeather.js
@@ -3,7 +3,12 @@ import { userLocation, weatherForecast } from '../api'
 
 export default async function fetchWeather() {
   // Get geo location from browser API, ignore errors
-  const geoPosition = await getGeoLocation()
+  let geoPosition
+  try {
+    geoPosition = await getGeoLocation()
+  } catch(err) {
+    console.warn(err)
+  }
 
   // Find user's location in server (by browser geo location or by ip)
   const location = await userLocation.getData({
